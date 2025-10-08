c30b08f30af3df6b69cf2ebc276c95d17e53a0c0
git remote add origin https://github.com/Mandchouno/ML_Kaggle.gitgit branch -M maingit push -u origin ma <TonEmail@exemple.com>
2025-10-08 19:16:11 -0400

    Final test

--- core/src/test/java/com/graphhopper/reader/dem/MultiSourceElevationProviderTest.java
+++ core/src/test/java/com/graphhopper/reader/dem/MultiSourceElevationProviderTest.java
@@ -31,7 +31,107 @@ public class MultiSourceElevationProviderTest {
 
     @AfterEach
     public void tearDown() {
-        instance.release();
+        if (instance != null)
+            instance.release();
+    }
+
+    /**
+     * [TEST 1] Frontière nord inclusive
+     * Vérifier que, à la frontière supérieure de couverture SRTM/CGIAR, le provider
+     * GMTED est bien utilisé.
+     * Données : lat=+60.0, lon=0.
+     * Valeur renvoyée par la méthode : 2.0 (=> GMTED)
+     */
+    @Test
+    public void delegatesToSecondProvider_atNorthBoundaryInclusive() {
+        instance = new MultiSourceElevationProvider(
+                new CGIARProvider() {
+                    @Override
+                    public double getEle(double lat, double lon) {
+                        return 1.0;
+                    }
+                },
+                new GMTEDProvider() {
+                    @Override
+                    public double getEle(double lat, double lon) {
+                        return 2.0;
+                    }
+                });
+        assertEquals(2.0, instance.getEle(60.0, 0.0), 0.1);
+    }
+
+    /**
+     * [TEST 2] Frontière sud inclusive
+     * Vérifier que, à la frontière inférieure de couverture SRTM/CGIAR, le provider
+     * GMTED est bien utilisé.
+     * Données : lat=-56.0, lon=0.
+     * Valeur renvoyée par la methode : 2.0 (=> GMTED).
+     */
+    @Test
+    public void delegatesToSecondProvider_atSouthBoundaryInclusive() {
+        instance = new MultiSourceElevationProvider(
+                new CGIARProvider() {
+                    @Override
+                    public double getEle(double lat, double lon) {
+                        return 1.0;
+                    }
+                },
+                new GMTEDProvider() {
+                    @Override
+                    public double getEle(double lat, double lon) {
+                        return 2.0;
+                    }
+                });
+        assertEquals(2.0, instance.getEle(-56.0, 0.0), 0.1);
+    }
+
+    /**
+     * [TEST 3] Hors couverture au nord
+     * Valider que le provider GMTED est bien choisie au-delà de la frontière.
+     * Données : lat=+60.0001, lon=0.
+     * Valeur renvoyée par la méthode : 2.0 (=> GMTED).
+     */
+    @Test
+    public void northJustOutside_usesGmted() {
+        instance = new MultiSourceElevationProvider(
+                new CGIARProvider() {
+                    @Override
+                    public double getEle(double lat, double lon) {
+                        return 1.0;
+                    }
+                },
+                new GMTEDProvider() {
+                    @Override
+                    public double getEle(double lat, double lon) {
+                        return 2.0;
+                    }
+                });
+        assertEquals(2.0, instance.getEle(60.0001, 0.0), 0.1);
+    }
+
+    /**
+     * [TEST 4] Hors couverture au sud
+     * Valider que le provider GMTED est bien choisie juste en-dessous de la
+     * frontière.
+     * Données : lat=-56.0001, lon=0.
+     * Valeur renvoyée par la méthode : 2.0 (=> GMTED).
+     */
+    @Test
+    public void delegatesToSecondProvider_justBeyondSouthBoundary() {
+        instance = new MultiSourceElevationProvider(
+                new CGIARProvider() {
+                    @Override
+                    public double getEle(double lat, double lon) {
+                        return 1.0;
+                    }
+                },
+                new GMTEDProvider() {
+                    @Override
+                    public double getEle(double lat, double lon) {
+                        return 2.0;
+                    }
+                });
+        assertEquals(2.0, instance.getEle(-56.0001, 0.0), 0.1);
     }
 
     @Test
@@ -48,8 +148,7 @@ public class MultiSourceElevationProviderTest {
                     public double getEle(double lat, double lon) {
                         return 2;
                     }
-                }
-        );
+                });
 
         assertEquals(1, instance.getEle(0, 0), .1);
         assertEquals(2, instance.getEle(60.0001, 0), .1);
@@ -57,13 +156,16 @@ public class MultiSourceElevationProviderTest {
     }
 
     /*
-    Enabling this test requires you to change the pom.xml and increase the memory limit for running tests.
-    Change to: <argLine>-Xmx500m -Xms500m</argLine>
-    */
+     * Enabling this test requires you to change the pom.xml and increase the memory
+     * limit for running tests.
+     * Change to: <argLine>-Xmx500m -Xms500m</argLine>
+     */
     @Disabled
     @Test
     public void testGetEle() {
-        instance = new MultiSourceElevationProvider();
+        instance = new MultiSourceElevationProvider(
+                new CGIARProvider(),
+                new GMTEDProvider());
         double precision = .1;
         // The first part is copied from the SRTMGL1ProviderTest
         assertEquals(338, instance.getEle(49.949784, 11.57517), precision);
@@ -79,7 +181,8 @@ public class MultiSourceElevationProviderTest {
         assertEquals(1107, instance.getEle(47.467753, 14.573911), precision);
         assertEquals(1930, instance.getEle(46.468835, 12.578777), precision);
         assertEquals(844, instance.getEle(48.469123, 9.576393), precision);
-        // The file for this coordinate does not exist, but there is a ferry tagged in OSM
+        // The file for this coordinate does not exist, but there is a ferry tagged in
+        // OSM
         assertEquals(0, instance.getEle(56.4787319, 17.6118363), precision);
         assertEquals(0, instance.getEle(56.4787319, 17.6118363), precision);
         // The second part is copied from the GMTEDProviderTest
